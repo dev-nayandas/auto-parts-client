0e9bfc80ed45df37535ed561dfe1838fba495b6f
dev-nayandas <rich.nayan@gmail.com>
2022-05-22 04:14:47 +0600

    navbar modified

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import NavBar from './Pages/Shared/NavBar';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import Footer from './Pages/Shared/Footer';
+import Login from './Pages/Home/Login/Login';
+import Blogs from './Pages/Home/Blogs';
 
 function App() {
   return (
@@ -11,6 +13,9 @@ function App() {
       <NavBar></NavBar>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/blogs" element={<Blogs/>} />
         {/* <Route path="about" element={<About />} /> */}
       </Routes>
       <Footer></Footer>
