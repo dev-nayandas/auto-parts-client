dc9ec10ce5f1bd03716645d3000143997888b15a
dev-nayandas <rich.nayan@gmail.com>
2022-05-23 02:29:31 +0600

    google sign in implimented

--- src/Pages/Home/Login/Login.js
+++ src/Pages/Home/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if(user){
+        console.log(user)
+    }
     return (
-        <div>
-            <h1>This is login page</h1>
+        <div className='flex  justify-center items-cneter'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center text-2xl font-bold">Login</h2>
+
+                    <div class="divider">OR</div>
+                    <button onClick={() => signInWithGoogle()} class="btn">Continue With Google</button>
+                    
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Shared/NavBar.js
+++ src/Pages/Shared/NavBar.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
     const menuItems = <>
         <li><Link to='/home'>Home</Link></li>
-        <li><Link to='/login'>Login</Link></li>
         <li><Link to='/blogs'>Blogs</Link></li>
+        <li><Link to='/login'>Login</Link></li>
 
         
     </>
