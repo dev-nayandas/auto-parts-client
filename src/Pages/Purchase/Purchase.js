9ebf3a1c42bf019fed87d37056c9c3c6c1919653
dev-nayandas <rich.nayan@gmail.com>
2022-05-23 16:58:27 +0600

    few issue solved

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Purchase = () => {
-    
+    const {partId} = useParams();
+    const {part, setPart} = useState()
+    useEffect(()=>{
+        // const url = `parts.json/${partId}`;
+        fetch(`parts.json/${partId}`)
+        .then(res => res.json())
+        .then(data =>setPart(data))
+    },[])
     return (
         <div class="card w-96 bg-base-500 shadow-xl  ">
             <div class="card-body ">
-                <h2 class="card-title">Card title!</h2>
+                <h2 class="card-title">{part}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
                 <p>User name</p>
                 <p>Email</p>
